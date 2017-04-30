4fb0f81e040ce648fa878566b9d5cac1c2e9a268
Cynthia Henaff <cynthia@henaff.io>
2017-04-30 23:08:50 +0200

    Add sounds to all buttons

--- index.ios.js
+++ index.ios.js
@@ -14,53 +14,100 @@ import {
   TouchableOpacity,
 } from 'react-native';
 
-var Sound = require('react-native-sound');
-// import Sound from 'react-native-sound';
+import Sound from 'react-native-sound';
 
 export default class ProjectApp1 extends Component {
   buttonGretchen() {
-    // alert('Grosse tête de Gretchen')
+    // alert('Grosse chieuse')
     Sound.setCategory('Playback')
-    const sound = new Sound('miaouGretchen.wav', Sound.MAIN_BUNDLE, (error) => { if (error) { alert(JSON.stringify(error)) } })
-    sound.play((res) => alert(res))
-}
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={{
-          flex: 1,
-          flexDirection: 'row'
-        }}>
-          <TouchableOpacity onPress={this.buttonGretchen} style={{flex: 1}}>
-            <Image
-              source={require('./img/gretchen.jpg')}
-              style={{flex: 1, width: null, height: null}}
-              resizeMode="cover"
-            />
-          </TouchableOpacity>
-            <Image
-              source={require('./img/baguette.jpg')}
-              style={{flex: 1, width: null, height: null}}
-            />
-            </View>
-        <View style={{
-          flex: 1,
-          flexDirection: 'row'
-        }}>
-          <Image
-            source={require('./img/petitNain.jpg')}
-            style={{flex: 1, width: null, height: null}}
-          />
-          <Image
-            source={require('./img/helga.jpg')}
-            style={{flex: 1, width: null, height: null}}
-          />
-        </View>
-      </View>
-    );
+    const soundGretchen = new Sound('miaulementGretchen.mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        alert(JSON.stringify(error));
+        return;
+      }
+      soundGretchen.play()
+    })
   }
-}
+
+    buttonHelga() {
+     Sound.setCategory('Playback')
+     const soundHelga = new Sound('miaouHelga.mp3', Sound.MAIN_BUNDLE, (error) => {
+      if (error) {
+        alert(JSON.stringify(error));
+        return;
+      }
+      soundHelga.play()
+      })
+    }
+    buttonMagicWand() {
+      Sound.setCategory('Playback')
+      const soundMagicWand = new Sound('fairyBell.mp3', Sound.MAIN_BUNDLE, (error) => {
+        if (error) {
+          alert(JSON.stringify(error));
+          return;
+        }
+        soundMagicWand.play()
+      })
+    }
+
+    buttonAdrien() {
+      Sound.setCategory('Playback')
+      const soundAdrien = new Sound('raspAdrien.mp3', Sound.MAIN_BUNDLE, (error) => {
+        if (error) {
+          alert(JSON.stringify(error));
+          return;
+        }
+        soundAdrien.play()
+      })
+    }
+
+    render() {
+        return (
+          <View style={styles.container}>
+            <View style={{
+              flex: 1,
+              flexDirection: 'row'
+              }}
+            >
+              <TouchableOpacity onPress={this.buttonGretchen} style={{flex: 1}}>
+                <Image
+                  source={require('./img/gretchen.jpg')}
+                  style={{flex: 1, width: null, height: null}}
+                  resizeMode="cover"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={this.buttonMagicWand} style={{flex: 1}}>
+                <Image
+                  source={require('./img/baguette.jpg')}
+                  style={{flex: 1, width: null, height: null}}
+                  resizeMode="cover"
+                />
+              </TouchableOpacity>
+            </View>
+            <View style={{
+              flex: 1,
+              flexDirection: 'row'
+              }}
+            >
+              <TouchableOpacity onPress={this.buttonAdrien} style={{flex: 1}}>
+                <Image
+                  source={require('./img/petitNain.jpg')}
+                  style={{flex: 1, width: null, height: null}}
+                  resizeMode="cover"
+                />
+              </TouchableOpacity>
+              <TouchableOpacity onPress={this.buttonHelga} style={{flex: 1}}>
+                <Image
+                  source={require('./img/helga.jpg')}
+                  style={{flex: 1, width: null, height: null}}
+                  resizeMode="cover"
+                />
+              </TouchableOpacity>
+            </View>
+          </View>
+        );
+      }
+    }
 
 const styles = StyleSheet.create({
   container: {
