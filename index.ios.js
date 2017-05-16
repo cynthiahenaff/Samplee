13d9355784218aee7860932c795565c0ed2e6f8f
Cynthia Henaff <cynthia@henaff.io>
2017-05-16 19:44:04 +0200

    Code refactorization

--- index.ios.js
+++ index.ios.js
@@ -12,54 +12,37 @@ import {
   View,
   Image,
   TouchableOpacity,
+  CameraRoll,
+  ImagePickerIOS,
+  Modal,
+  TouchableHighlight,
+  TextInput
 } from 'react-native';
 
 import Sound from 'react-native-sound';
+import ModalPicker from 'react-native-modal-picker';
+// import Camera from 'react-native-camera';
+
 
 export default class ProjectApp1 extends Component {
-  buttonGretchen() {
-    // alert('Grosse chieuse')
-    Sound.setCategory('Playback')
-    const soundGretchen = new Sound('miaulementGretchen.mp3', Sound.MAIN_BUNDLE, (error) => {
-      if (error) {
-        alert(JSON.stringify(error));
-        return;
-      }
-      soundGretchen.play()
-    })
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: null
+    }
   }
 
-    buttonHelga() {
-     Sound.setCategory('Playback')
-     const soundHelga = new Sound('miaouHelga.mp3', Sound.MAIN_BUNDLE, (error) => {
+  soundPlay(soundName) {
+    Sound.setCategory('Playback')
+    const sound = new Sound(soundName, Sound.MAIN_BUNDLE, (error) => {
       if (error) {
         alert(JSON.stringify(error));
         return;
       }
-      soundHelga.play()
-      })
-    }
-    buttonMagicWand() {
-      Sound.setCategory('Playback')
-      const soundMagicWand = new Sound('fairyBell.mp3', Sound.MAIN_BUNDLE, (error) => {
-        if (error) {
-          alert(JSON.stringify(error));
-          return;
-        }
-        soundMagicWand.play()
-      })
-    }
-
-    buttonAdrien() {
-      Sound.setCategory('Playback')
-      const soundAdrien = new Sound('raspAdrien.mp3', Sound.MAIN_BUNDLE, (error) => {
-        if (error) {
-          alert(JSON.stringify(error));
-          return;
-        }
-        soundAdrien.play()
-      })
-    }
+      sound.play()
+    })
+  }
 
     render() {
         return (
@@ -69,34 +52,39 @@ export default class ProjectApp1 extends Component {
               flexDirection: 'row'
               }}
             >
-              <TouchableOpacity onPress={this.buttonGretchen} style={{flex: 1}}>
+              <View style={{flex: 1}}>
+
+              <TouchableOpacity>
                 <Image
-                  source={require('./img/gretchen.jpg')}
-                  style={{flex: 1, width: null, height: null}}
-                  resizeMode="cover"
+                  source={require('./img/logoPhoto.png')}
+                  style={{width: 50, height:50}}
                 />
               </TouchableOpacity>
-              <TouchableOpacity onPress={this.buttonMagicWand} style={{flex: 1}}>
+              </View>
+
+              <View style={{flex: 1}}>
+              <TouchableOpacity onPress={this.soundPlay.bind(this, 'fairyBell.mp3')} style={{flex: 1}}>
                 <Image
                   source={require('./img/baguette.jpg')}
                   style={{flex: 1, width: null, height: null}}
                   resizeMode="cover"
                 />
               </TouchableOpacity>
+              </View>
             </View>
             <View style={{
               flex: 1,
               flexDirection: 'row'
               }}
             >
-              <TouchableOpacity onPress={this.buttonAdrien} style={{flex: 1}}>
+              <TouchableOpacity onPress={this.soundPlay.bind(this, 'raspAdrien.mp3')} style={{flex: 1}}>
                 <Image
                   source={require('./img/petitNain.jpg')}
                   style={{flex: 1, width: null, height: null}}
                   resizeMode="cover"
                 />
               </TouchableOpacity>
-              <TouchableOpacity onPress={this.buttonHelga} style={{flex: 1}}>
+              <TouchableOpacity onPress={this.soundPlay.bind(this, 'miaouHelga.mp3')} style={{flex: 1}}>
                 <Image
                   source={require('./img/helga.jpg')}
                   style={{flex: 1, width: null, height: null}}
@@ -114,18 +102,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'pink'
+    backgroundColor: 'white'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
+})
 
 AppRegistry.registerComponent('ProjectApp1', () => ProjectApp1);
