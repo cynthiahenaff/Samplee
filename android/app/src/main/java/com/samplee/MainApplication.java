9a12f04cbf21e27ae2db1f0bc28b4c50fff75f71
Cynthia Henaff <cynthia@henaff.io>
2017-11-10 18:12:49 +0100

    Upgrade React-native & resolve a bug with react-native-svg-uri (issue: https://github.com/matc4/react-native-svg-uri/issues/87)

--- android/app/src/main/java/com/samplee/MainApplication.java
+++ android/app/src/main/java/com/samplee/MainApplication.java
@@ -35,6 +35,11 @@ public class MainApplication extends Application implements ReactApplication {
             new RCTCameraPackage()
       );
     }
+
+    @Override
+    protected String getJSMainModuleName() {
+      return "index";
+    }
   };
 
   @Override
--- ios/Samplee/AppDelegate.m
+++ ios/Samplee/AppDelegate.m
@@ -20,10 +20,17 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-  
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-  
+
   // For react-native-navigation
+
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
+                                                      moduleName:@"Samplee"
+                                               initialProperties:nil
+                                                   launchOptions:launchOptions];
+  rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
+
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   self.window.backgroundColor = [UIColor whiteColor];
   [[RCCManager sharedInstance] initBridgeWithBundleURL:jsCodeLocation launchOptions:launchOptions];
